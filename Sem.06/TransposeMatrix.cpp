317a57d67bc0fd7acf68b5374fed6ab18a76d906
Atanas Hristov <44443630+AtanasHrist0v@users.noreply.github.com>
2025-11-21 00:00:43 +0200

    Add files via upload

---
+++ Sem.06/TransposeMatrix.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+const int MATRIX_MAX_SIZE = 100;
+
+void swap(int& a, int& b) {
+	int temp = a;
+	a = b;
+	b = temp;
+}
+
+void transpose(int matrix[MATRIX_MAX_SIZE][MATRIX_MAX_SIZE], unsigned N, unsigned M) {
+	if (N < 1 || M < 1) {
+		std::cout << "matrix has invalid size";
+		return;
+	}
+
+	for (int i = 0; i < N; i++) {
+		for (int j = i + 1; j < M; j++) {
+			swap(matrix[i][j], matrix[j][i]);
+		}
+	}
+}
